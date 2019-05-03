618b7fe901c261e8c84200190530dce83a1ef6cb
zhmiao <zhongqi.miao@berkeley.edu>
2019-05-02 18:41:31 -0700

    debug imagenet stage1 configuration file

--- config/ImageNet_LT/stage_1.py
+++ config/ImageNet_LT/stage_1.py
@@ -11,15 +11,14 @@ training_opt['num_epochs'] = 30
 training_opt['display_step'] = 10
 training_opt['feature_dim'] = 512
 training_opt['open_threshold'] = 0.1
-training_opt['sampler'] = {'type': 'ClassAwareSampler', 'def_file': './data/ClassAwareSampler.py',
-                           'num_samples_cls': 4}
+training_opt['sampler'] = None
 training_opt['scheduler_params'] = {'step_size':10, 'gamma':0.1}
 config['training_opt'] = training_opt
 
 networks = {}
 feature_param = {'use_modulatedatt':False, 'use_fc': True, 'dropout': None,
 			  'stage1_weights': False, 'dataset': training_opt['dataset'], 'caffe': False}
-feature_optim_param = {'lr': 0.01, 'momentum':0.9, 'weight_decay':0.0005}
+feature_optim_param = {'lr': 0.1, 'momentum':0.9, 'weight_decay':0.0005}
 networks['feat_model'] = {'def_file': './models/ResNet10Feature.py',
                           'params': feature_param,
                           'optim_params': feature_optim_param,
